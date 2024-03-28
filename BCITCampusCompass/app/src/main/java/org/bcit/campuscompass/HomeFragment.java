5a33fa810e6341a797b59f0b5246526389029d72
EddieSherban <109121401+EddieSherban@users.noreply.github.com>
2024-03-28 15:15:36 -0700

    No more db errors

--- BCITCampusCompass/app/src/main/java/org/bcit/campuscompass/HomeFragment.java
+++ BCITCampusCompass/app/src/main/java/org/bcit/campuscompass/HomeFragment.java
@@ -1,19 +1,56 @@
 package org.bcit.campuscompass;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.bcit.campuscompass.DatabaseHelper;
+import org.bcit.campuscompass.R;
+
+import java.io.IOException;
 
 public class HomeFragment extends Fragment {
+
+    private TextView debugTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //find the debug TextView
+        debugTextView = rootView.findViewById(R.id.debugTextView);
+
+        //debug message
+        //String debugMessage = "Latitude and Longitude: " + getValuetoDebug("Burnaby Campus");
+
+        // set debug message to the textview
+        debugTextView.setText(getValuetoDebug("lol"));
+        debugTextView.setVisibility(View.VISIBLE);
+
+        return rootView;
+
     }
+
+    public String getValuetoDebug(String campusName) {
+        DatabaseHelper dbHelper = new DatabaseHelper(requireContext());
+        try {
+            dbHelper.copyDatabase();
+            dbHelper.openDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return dbHelper.getRoomDimensions();
+    }
+
+    // hi michael here
 }
